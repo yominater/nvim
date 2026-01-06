870ba3c8d3b0e4e2f9e63d8cf3608f5b12e54db5
yominater <alexandertrains4@gmail.com>
2026-01-06 15:03:46 -0500

    Cleaned up config

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,11 +4,10 @@ return {
   { "lewis6991/gitsigns.nvim",  enabled = false },
   { "folke/flash.nvim",         enabled = true },  --better searching with '/''
   { "folke/lazydev.nvim",       enabled = true },  --lazily updating your workspace libraries.
-  { "echasnovski/mini.nvim",    enabled = false }, --support for special characters?
+  { "nvim-mini/mini.nvim",    enabled = false }, --support for special characters?
   { "folke/noice.nvim",         enabled = true },  --for fancy popup menus
   { "folke/persistence.nvim",   enabled = false }, --for persistant sessions
   { "folke/todo-comments.nvim", enabled = false }, --highlight TODO
   { "folke/trouble.nvim",       enabled = false }, --sidebar for errors
   { "folke/ts-comments.nvim",   enabled = false }, --custom syntax for comments
-  { "echasnovski/mini.pairs",   enabled = false },
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,12 +14,6 @@ return {
     end,
   },
   {
-    'bettervim/yugen.nvim',
-    config = function()
-      -- vim.cmd.colorscheme('yugen')
-    end,
-  },
-  {
     "morhetz/gruvbox",
     config = function()
       vim.cmd.colorscheme('gruvbox')
@@ -32,20 +26,6 @@ return {
            highlight FoldColumn guibg=#000000
          ]]
     end,
-  },
-  {
-    "catppuccin/nvim",
-    -- lazy = false,
-    name = "catppuccin",
-    -- priority = 1000,
-    config = function()
-      --vim.cmd.colorscheme "catppuccin-macchiato"
-      require("catppuccin").setup({
-        custom_highlights = {
-          ["@markup.italic.markdown_inline"] = { italic = true, bold = true, fg = "#93e7ed" }
-        }
-      })
-    end
   },
 
 }
